8d7c6e4f184416dc2437a05490c0d8240b4ede79
jva2jva2 <JVA2@pge.com>
2018-01-19 15:34:39 -0800

    fixed main

--- git2.java
+++ git2.java
@@ -3,11 +3,7 @@ public class git2
 {
 	public static void main( String[] args )
 	{
-		System.out.println( "Hello Git!" );
-		System.out.println( "First change" );
-		System.out.println( "Second change" );
-		System.out.println( "Third change" );
-		System.out.println( "Fixed main" );
+		System.out.println( "---- This is the output from git2.java ------" );
 	}
 }
 
